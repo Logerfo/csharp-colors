c78282826bfce4bcff7156a5a139bf5aac21f9d3
Bruno Logerfo <bclogerfo@hotmail.com>
2019-07-30 17:58:46 -0300

    int adjustments
    fixed not allowing leading +
    not parsing NaN (hex without 0x, 0b with other than 0 or 1, etc)
    more succinct code

--- src/strategies/int.ts
+++ src/strategies/int.ts
@@ -1,30 +1,34 @@
 import Color = require("color");
 
-const hexRegex = /(Color\s*\.\s*FromArgb\s*\()\s*(0[xXbB])?([0-9a-fA-F_]+)\s*\)/g;
+const hexRegex = /(Color\s*\.\s*FromArgb\s*\()\s*\+?\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*\)/g;
 
 export async function findInt(text) {
     let match = hexRegex.exec(text);
     let result = [];
 
     while (match !== null) {
+        function parseArgument(index: number): number {
+            let hexModifier = match[index];
+            if (hexModifier) {
+                hexModifier = hexModifier.toUpperCase();
+                return parseInt(match[index + 1].replace("_", ""), 16);
+            }
+            let binaryModifier = match[index + 2];
+            if (binaryModifier) {
+                binaryModifier = binaryModifier.toUpperCase();
+                return parseInt(match[index + 3].replace("_", ""), 2);
+            }
+            let dec = match[index + 4];
+            if (dec) {
+                return +dec.replace("_", "");
+            }
+            return 255;
+        }
+
         const start = match.index + match[1].length;
         const end = hexRegex.lastIndex - 1; //cuts the close parenthesis
-        let modifier = match[2];
-        const value = match[3].replace("_", "");
-        let matchedColor: string;
-        if (modifier) {
-            modifier = modifier.toUpperCase();
-            if (modifier.startsWith("0X")) {
-                matchedColor = value;
-            } else if (modifier.startsWith("0B")) {
-                matchedColor = parseInt(value, 2).toString(16);
-            } else {
-                throw new Error("This is not supposed to happen");
-            }
-        }
-        else {
-            matchedColor = (+value).toString(16);
-        }
+
+        let matchedColor = parseArgument(2);
 
         try {
             const color = Color.rgb(`#${matchedColor}`).rgb().string();
