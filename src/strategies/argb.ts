ea0773268ffad54c88ca097cc7ac3fd988059ddb
Bruno Logerfo <bclogerfo@hotmail.com>
2019-07-30 17:41:05 -0300

    more argb adjustments
    not parsing NaN (hex without 0x, 0b with other than 0 or 1, etc)
    more succinct code

--- src/strategies/argb.ts
+++ src/strategies/argb.ts
@@ -1,86 +1,37 @@
 import Color = require("color");
 
-const argbRegex = /(Color\s*\.\s*FromArgb\s*\()\s*(?:(0[xXbB])?([0-9a-fA-F_]+)\s*,\s*)?(0[xXbB])?([0-9a-fA-F_]+)\s*,\s*(0[xXbB])?([0-9a-fA-F_]+)\s*,\s*(0[xXbB])?([0-9a-fA-F_]+)\s*\)/g;
+const argbRegex = /(Color\s*\.\s*FromArgb\s*\()\s*(?:(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*,\s*)?(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*,\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*,\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*\)/g;
 
 export async function findARGB(text) {
     let match = argbRegex.exec(text);
     let result = [];
 
     while (match !== null) {
+        function parseArgument(index: number): number {
+            let hexModifier = match[index];
+            if (hexModifier) {
+                hexModifier = hexModifier.toUpperCase();
+                return parseInt(match[index + 1].replace("_", ""), 16);
+            }
+            let binaryModifier = match[index + 2];
+            if (binaryModifier) {
+                binaryModifier = binaryModifier.toUpperCase();
+                return parseInt(match[index + 3].replace("_", ""), 2);
+            }
+            let dec = match[index + 4];
+            if (dec) {
+                return +dec.replace("_", "");
+            }
+            return 255;
+        }
+
         const start = match.index + match[1].length;
         const end = argbRegex.lastIndex - 1; //cuts the close parenthesis
-        let Amodifier = match[2];
-        let Avalue = match[3];
-        let matchedA: number;
-        if (Avalue) {
-            Avalue = match[3].replace("_", "");
-            if (Amodifier) {
-                Amodifier = Amodifier.toUpperCase();
-                if (Amodifier.startsWith("0X")) {
-                    matchedA = parseInt(Avalue, 16);
-                } else if (Amodifier.startsWith("0B")) {
-                    matchedA = parseInt(Avalue, 2);
-                } else {
-                    throw new Error("This is not supposed to happen");
-                }
-            }
-            else {
-                matchedA = +Avalue;
-            }
-        }
-        else {
-            matchedA = 255;
-        }
-        let Rmodifier = match[4];
-        const RValue = match[5].replace("_", "");
-        let matchedR: number;
-        if (Rmodifier) {
-            Rmodifier = Rmodifier.toUpperCase();
-            if (Rmodifier.startsWith("0X")) {
-                matchedR = parseInt(RValue, 16);
-            } else if (Rmodifier.startsWith("0B")) {
-                matchedR = parseInt(RValue, 2);
-            } else {
-                throw new Error("This is not supposed to happen");
-            }
-        }
-        else {
-            matchedR = +RValue;
-        }
 
-        let Gmodifier = match[6];
-        const Gvalue = match[7].replace("_", "");
-        let matchedG: number;
-        if (Gmodifier) {
-            Gmodifier = Gmodifier.toUpperCase();
-            if (Gmodifier.startsWith("0X")) {
-                matchedG = parseInt(Gvalue, 16);
-            } else if (Gmodifier.startsWith("0B")) {
-                matchedG = parseInt(Gvalue, 2);
-            } else {
-                throw new Error("This is not supposed to happen");
-            }
-        }
-        else {
-            matchedG = +Gvalue;
-        }
-
-        let Bmodifier = match[8];
-        const Bvalue = match[9].replace("_", "");
-        let matchedB: number;
-        if (Bmodifier) {
-            Bmodifier = Bmodifier.toUpperCase();
-            if (Bmodifier.startsWith("0X")) {
-                matchedB = parseInt(Bvalue, 16);
-            } else if (Bmodifier.startsWith("0B")) {
-                matchedB = parseInt(Bvalue, 2);
-            } else {
-                throw new Error("This is not supposed to happen");
-            }
-        }
-        else {
-            matchedB = +Bvalue;
-        }
+        let matchedA = parseArgument(2);
+        let matchedR = parseArgument(7);
+        let matchedG = parseArgument(12);
+        let matchedB = parseArgument(17);
 
         try {
             const color = Color(`rgba(${matchedR}, ${matchedG}, ${matchedB}, ${matchedA / 255})`).rgb().string();
