3483361144f95239fe4dc2d8ce3590ad74198aac
Bruno Logerfo <bclogerfo@hotmail.com>
2019-09-27 10:11:05 -0300

    Revert "using the same parser for argb and color32"
    This reverts commit 6a60cf30873a41c847861ab1298c012a2aedb8f3.

--- src/strategies/argb.ts
+++ src/strategies/argb.ts
@@ -1,7 +1,45 @@
-import { parse } from "./argb-parser";
+import Color = require("color");
 
 const argbRegex = /(Color\s*\.\s*FromArgb\s*\()\s*(?:\+?\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*,\s*)?\+?\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*,\s*\+?\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*,\s*\+?\s*(?:(0[xX])([0-9a-fA-F_]+)|(0[bB])([01_]+)|([0-9_]+))\s*\)/g;
 
 export async function findARGB(text) {
-    return parse(text, argbRegex)
+    let match = argbRegex.exec(text);
+    let result = [];
+
+    while (match !== null) {
+        function parseArgument(index: number): number {
+            let hexModifier = match[index];
+            if (hexModifier) {
+                hexModifier = hexModifier.toUpperCase();
+                return parseInt(match[index + 1].replace("_", ""), 16);
+            }
+            let binaryModifier = match[index + 2];
+            if (binaryModifier) {
+                binaryModifier = binaryModifier.toUpperCase();
+                return parseInt(match[index + 3].replace("_", ""), 2);
+            }
+            let dec = match[index + 4];
+            if (dec) {
+                return +dec.replace("_", "");
+            }
+            return 255;
+        }
+
+        const start = match.index + match[1].length;
+        const end = argbRegex.lastIndex - 1; //cuts the close parenthesis
+
+        let matchedA = parseArgument(2);
+        let matchedR = parseArgument(7);
+        let matchedG = parseArgument(12);
+        let matchedB = parseArgument(17);
+
+        try {
+            const color = Color(`rgba(${matchedR}, ${matchedG}, ${matchedB}, ${matchedA / 255})`).rgb().string();
+            result.push({ start, end, color });
+        }
+        catch { }
+
+        match = argbRegex.exec(text);
+    }
+    return result;
 }
